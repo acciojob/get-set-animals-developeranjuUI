a3be245729ad580429211115effe5f2b785e9ed9
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-25 14:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	private species: string;
+	constructor(species: string){
+		this.species;
+	}
+	get species(): string{
+		return this.species;
+	}
+	makeSound(): void{
+		console.log(`The ${this.species} makes a sound`)
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	purr(): void{
+		console.log("purr");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	bark(): void{
+		console.log("woof");
+	}
+}
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+
+myCat.purr();
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+
+myDog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
